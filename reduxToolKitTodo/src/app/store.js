1ca8a9014789ad2e43fc091d1fbf9fc1817798f1
Azizkhan93 <aziz_khan19@hotmail.com>
2025-09-24 14:07:59 +0500

    Todo With Redux & Tedux ToolKit

---
+++ reduxToolKitTodo/src/app/store.js
@@ -0,0 +1,10 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit'
+import todoReducer from '../features/todo/todoSlice'
+
+export const store = configureStore({
+
+    reducer: todoReducer
+})
+
+
+
